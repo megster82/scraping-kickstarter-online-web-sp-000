415981c96d8122469b22abd2cbdbeaf07ad377cb
meaghan jennings <meaghanljennings@gmail.com>
2021-04-02 17:43:46 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,10 +3,10 @@ require "pry"
 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# image link: project.css("div.project-thumbnail a img").attribute("src").value 
 # description: project.css("p.bbcard_blurb").text 
-# description: project.css("span.location-name").text
-# percent_funded: project.css(ul.project-stats li.first.funded strong").text.gsub("%", "").to_i 
+# location: project.css("span.location-name").text 
+# % funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i  
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
@@ -15,15 +15,17 @@ def create_project_hash
   projects = {}
   
   kickstarter.css("li.project.grid_4").each do |project|
-    title = project.css("h2.bbcard_name strong a").text
+    title = project.css("h2.bbcard_name strong a").text 
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("ul.project-meta span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-      }
-  end 
-  #return the projects hash
+    }
+  end
   
-  projects
+  # return the projects hash 
+  projects 
 end
+
+create_project_hash
